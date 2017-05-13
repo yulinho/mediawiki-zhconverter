bd7aa8547a73b36a75322cf19c40cd4bbff54f37
YULIN HE <YULIN HE>
2017-05-14 02:52:21 +0800

    Signed-off-by: YULIN HE <YULIN HE>

--- example.php
+++ example.php
@@ -6,10 +6,10 @@
  * YOU SHOULD PLACE THEM OUTSIDE THE DOCUMENT ROOT OF YOUR WEB SERVER!!!
  */
 
-define("MEDIAWIKI_PATH", "/var/lib/mediawiki-1.26.2/");
+define("MEDIAWIKI_PATH", "mediawiki-1.26.2");
 
 /* Include our helper class */
-require_once "mediawiki-zhconverter.inc.php";
+// require_once "mediawiki-zhconverter.inc.php";
 
 /* Convert it, valid variants such as zh, zh-hans, zh-hant, zh-cn, zh-tw, zh-sg & zh-hk */
 echo MediaWikiZhConverter::convert("雪糕", "zh-tw") , ",";
